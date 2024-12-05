d77806af3ca576fd50c7486522e8a342d55d1d71
Rajmate16 <97882958+Rajmate16@users.noreply.github.com>
2024-12-05 12:31:10 +0530

    Delete Airline-App-Java-master/Airline-App-Java-master directory

--- Airline-App-Java-master/Airline-App-Java-master/src/main/java/com/airline/repository/FlightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.airline.repository;
-
-import com.airline.model.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlightRepository extends JpaRepository<Flight,Integer> {
-}
